940eecdda9eb5b1c45a530f2073e15534fb39ca8
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-09-24 19:33:42 +0300

    homework 3 after after task 1

---
+++ src/main/homework3/task1/Calculator.java
@@ -0,0 +1,19 @@
+package main.homework3.task1;
+
+public class Calculator {
+    int summ(int a, int b) {
+        return a + b;
+    }
+
+    int substract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        return a / b;
+    }
+}
