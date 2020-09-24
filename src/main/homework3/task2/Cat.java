4567abf59d821352285cc4d7417c51301fc7830c
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-09-24 22:29:48 +0300

    homework 3 after after task 2-bez sortirovki

---
+++ src/main/homework3/task2/Cat.java
@@ -0,0 +1,13 @@
+package main.homework3.task2;
+
+public class Cat {
+    int age;
+    int weight;
+    String name;
+
+    public Cat(int age, int weight, String name) {
+        this.age = age;
+        this.weight = weight;
+        this.name = name;
+    }
+}
