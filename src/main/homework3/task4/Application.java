50759e548db10e386687ce8a40a99ea4a4fd81bf
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-09-26 14:48:56 +0300

    homework 3 in the task 4

--- src/main/homework3/task4/Application.java
+++ src/main/homework3/task4/Application.java
@@ -1,4 +1,15 @@
 package main.homework3.task4;
 
 public class Application {
+    public static void main(String[]  args){
+     Human sergey=new Human{"Sergey","Valeriy","Grigoriy"};
+     Human veleriy=new Human{"Valeriy","Sergey","Stanislav"};
+     Human Stanislav=new Human{"Stanislav","Valeriy","Mihail"};
+     Human mihail=new Human{"Mihail","Stanislav","Aleksandr"};
+     Human aleksandr=new Human{"Aleksandr","Mihail","Egorom","Grigoriy"};
+     Human egor=new Human{"Egor","Aleksandr","Grigoriy"};
+     Human grigoriy=new Human{"Grigoriy","Aleksandr","Egor","Sergey"};
+
+
+    }
 }
--- src/main/homework3/task4/Human.java
+++ src/main/homework3/task4/Human.java
@@ -1,4 +1,14 @@
 package main.homework3.task4;
 
 public class Human {
+    String name;
+    Human[] friends=new Human[7];
+    int count=0;
+    public Human(String name, Human ...friends) {
+        this.name=name;
+        this.friends=friends;
+        for(Human human:friends){
+            friends[count++]=human;
+        }
+    }
 }
