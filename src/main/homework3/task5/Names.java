989208c83cd6898a35d9e4a2fee201fab6ec47fd
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-09-27 13:13:16 +0300

    homework 3 after 5

---
+++ src/main/homework3/task5/Names.java
@@ -0,0 +1,21 @@
+package main.homework3.task5;
+
+import java.util.Random;
+
+public class Names {
+    static String[] humanNames = {"Oleg", "Vasiliy", "Ivan", "Katja", "Natalya"};
+    static String[] catNames = {"Murzik", "Kisakisa", "Mono"};
+    static String[] dogNames = {"Gavgav", "Sharik", "Bobik"};
+
+    static String humanName() {
+        return humanNames[new Random().nextInt(humanNames.length)];
+    }
+
+    static String catName() {
+        return catNames[new Random().nextInt(catNames.length)];
+    }
+
+    static String dogName() {
+        return catNames[new Random().nextInt(catNames.length)];
+    }
+}
