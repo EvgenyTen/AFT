06f61deb15442b7d091279d213d9176985985ae9
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-01 14:19:51 +0300

    homework 3 after 5 pravki

--- src/main/homework3/task1/Calculator.java
+++ src/main/homework3/task1/Calculator.java
@@ -1,19 +1,19 @@
 package main.homework3.task1;
 
 public class Calculator {
-    int summ(int a, int b) {
+    static int summ(int a, int b) {
         return a + b;
     }
 
-    int substract(int a, int b) {
+    static int substract(int a, int b) {
         return a - b;
     }
 
-    int multiply(int a, int b) {
+    static int multiply(int a, int b) {
         return a * b;
     }
 
-    int divide(int a, int b) {
+    static int divide(int a, int b) {
         return a / b;
     }
 }
