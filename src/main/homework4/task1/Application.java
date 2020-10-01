2f6f57b72508f98fccb6a86654e0e705b5faa6f8
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-01 17:59:11 +0300

    homework 4

---
+++ src/main/homework4/task1/Application.java
@@ -0,0 +1,12 @@
+package main.homework4.task1;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Human human = new Human();
+
+            }
+        }
+    }
+
+}
---
+++ src/main/homework4/task1/Human.java
@@ -0,0 +1,22 @@
+package main.homework4.task1;
+
+public class Human {
+    private String firstName,secondName;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+}
