1d27b0ccae714d07a41af427327535cdf2abbc27
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-03 18:30:23 +0300

    homework 4 after task  4

--- src/main/homework4/task4/Application.java
+++ src/main/homework4/task4/Application.java
@@ -2,12 +2,10 @@ package main.homework4.task4;
 
 public class Application {
     public static void main(String[] args) {
-        Passport passport = new Passport().setSeries("0000").setNumber("000000");
-        Passport notUniquePassport = new Passport().setSeries("0000").setNumber("000000");
-        Passport incorrectPassport = new Passport().setSeries("123").setNumber("12555");
-        Human human = new Human().setName("Иван").setSurname("Иванов").setAge(15).setPassport(passport);
-
-
+        Passport passport = new Passport().setSeries("3333").setNumber("333333");
+        Passport notUniquePassport = new Passport().setSeries("3333").setNumber("333333");
+        Passport incorrectPassport = new Passport().setSeries("111").setNumber("11111");
+        Human human = new Human().setName("Иван").setSurname("Иванов").setAge(35).setPassport(passport);
     }
 
 }
