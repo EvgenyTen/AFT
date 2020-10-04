443c5eb4070ea6b5dcfac255b665fe7f572a3ed1
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-04 15:01:09 +0300

    homework 5 after task  4

--- src/main/homework5/task4/Application.java
+++ src/main/homework5/task4/Application.java
@@ -1,7 +1,31 @@
 package main.homework5.task4;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
+        Cat[] cats = new Cat[10];
 
+        for (int i = 0; i < cats.length; i++) {
+            try {
+                cats[i] = new Cat("Cat", new Random().nextInt(100) - 50);
+                System.out.println("Name : " + cats[i].name + ", Age: " + cats[i].age);
+            } catch (AgeTooHighException exception) {
+                cats[i] = null;
+                System.out.println("Too old");
+            } finally {
+                i++;
+            }
+        }
+
+        for (int i = 0; i < cats.length; i++) {
+            try {
+
+            } catch (NullPointerException npe) {
+                System.out.println("Error");
+            }
+        }
     }
+
 }
+
