24400edd45de3a5f9498fa80297a7d74aab8d8e5
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-09 22:17:54 +0300

    homework 6 task 1 begin

--- src/main/homework6/task1/Application.java
+++ src/main/homework6/task1/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
      System.out.println(NameGenerator.generate());
      System.out.println(PhoneGenerator.generate());
+     System.out.println(AgeGenerator.generate());
     }
 }
