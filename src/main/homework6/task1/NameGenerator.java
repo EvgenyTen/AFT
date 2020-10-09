abba7f82699912641fb286583cb17bbaf5c82e3d
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-09 20:36:52 +0300

    homework 6 task 1 begin

--- src/main/homework6/task1/NameGenerator.java
+++ src/main/homework6/task1/NameGenerator.java
@@ -4,7 +4,10 @@ import java.util.Random;
 
 public class NameGenerator {
     public static int generate() {
-        int age=new Random().nextInt(55)+20;
-        return age;
+        char []smallLetters={'а','б', 'в', 'г', 'д','е','ё','ж','з', 'и','й','к','л','м', 'н', 'о','п', 'р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю', 'я'};
+        char[] bigLetters={'А', 'Б','В', 'Г', 'Д','Е', 'Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ', 'Ы', 'Ь', 'Э','Ю','Я'};
+        int nameSize=new Random().nextInt(12)+5;
+        char[] name=new char[nameSize];
+        return namSize;
     }
 }
--- src/main/homework6/task1/PhoneGenerator.java
+++ src/main/homework6/task1/PhoneGenerator.java
@@ -6,6 +6,6 @@ public class PhoneGenerator {
     public static int generate() {
 
         int phone=new Random().nextInt(55)+20;
-        return ;
+        return phone ;
     }
 }
