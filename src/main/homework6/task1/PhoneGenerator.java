8659addd4ddc2d5611368d0a21ecbea95ca33c72
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-09 20:06:39 +0300

    homework 6 task 1 begin

--- src/main/homework6/task1/PhoneGenerator.java
+++ src/main/homework6/task1/PhoneGenerator.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class PhoneGenerator {
     public static int generate() {
-        int age=new Random().nextInt(55)+20;
-        return age;
+
+        int phone=new Random().nextInt(55)+20;
+        return ;
     }
 }
