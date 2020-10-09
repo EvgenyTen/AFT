f94b39bc1a43d249ab3c9c20e13f6069f0a2fd86
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-09 22:11:02 +0300

    homework 6 task 1 begin

--- src/main/homework6/task1/Application.java
+++ src/main/homework6/task1/Application.java
@@ -3,5 +3,6 @@ package main.homework6.task1;
 public class Application {
     public static void main(String[] args) {
      System.out.println(NameGenerator.generate());
+     System.out.println(PhoneGenerator.generate());
     }
 }
--- src/main/homework6/task1/PhoneGenerator.java
+++ src/main/homework6/task1/PhoneGenerator.java
@@ -3,9 +3,23 @@ package main.homework6.task1;
 import java.util.Random;
 
 public class PhoneGenerator {
-    public static int generate() {
 
-        int phone=new Random().nextInt(55)+20;
-        return phone ;
+    public static String generate() {
+        return String.format("+7(%s)%s-%s-%s", randomNumber(3), randomNumber(3), randomNumber(2), randomNumber(2));
+    }
+
+    private static String randomNumber(int length) {
+        int[] digitsForNumber = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] series = new int[length];
+        for (int i = 0; i < length; i++) {
+            series[i] = digitsForNumber[new Random().nextInt(10)];
+            ;
+        }
+        String stringNumber = "";
+        for (int value : series) {
+            stringNumber = String.format("%s%s", stringNumber, value);
+        }
+        return stringNumber;
     }
 }
+
