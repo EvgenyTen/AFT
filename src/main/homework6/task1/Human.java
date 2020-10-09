a4fef29ac0877b312e4e32eec23846d40de2c60a
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-09 22:14:48 +0300

    homework 6 task 1 begin

--- src/main/homework6/task1/Human.java
+++ src/main/homework6/task1/Human.java
@@ -1,4 +1,31 @@
 package main.homework6.task1;
 
 public class Human {
+    private String surname;
+    private String name;
+    private int age;
+    private String phone;
+
+    public Human(String lastName, String firstName, int age, String phone) {
+        this.surname = lastName;
+        this.name = firstName;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
