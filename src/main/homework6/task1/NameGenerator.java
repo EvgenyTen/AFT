c41f0d143d4abb798b02e006f841754b99d10e8a
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-09 21:03:17 +0300

    homework 6 task 1 begin

--- src/main/homework6/task1/NameGenerator.java
+++ src/main/homework6/task1/NameGenerator.java
@@ -3,15 +3,15 @@ package main.homework6.task1;
 import java.util.Random;
 
 public class NameGenerator {
-    public static int generate() {
+    public static char generate() {
         char []smallLetters={'а','б', 'в', 'г', 'д','е','ё','ж','з', 'и','й','к','л','м', 'н', 'о','п', 'р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю', 'я'};
         char[] bigLetters={'А', 'Б','В', 'Г', 'Д','Е', 'Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ', 'Ы', 'Ь', 'Э','Ю','Я'};
+        //* генерация размера имени*//
         int nameSize=new Random().nextInt(12)+5;
         char[] name=new char[nameSize];
-        int bigLetterNumber=new Random().nextInt(32);
-        name[0]=bigLetters[bigLetterNumber];
-       // for (int i=1;i<nameSize;i++){
-      //  }
+        //*генерация первого символа*//
+        name[0]=bigLetters[new Random().nextInt(32)];
+
         return name[0];
     }
 }
