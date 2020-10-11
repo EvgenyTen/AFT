c13789ebc48cfe62b07393c0e0021c642c7d74b5
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-10-11 18:11:16 +0300

    homework 6 task 4 finished

--- src/main/homework6/task2/Chance.java
+++ src/main/homework6/task2/Chance.java
@@ -1,9 +1,11 @@
 package main.homework6.task2;
 
+import java.util.Random;
+
 public class Chance {
     public static boolean calculateChance(int percent) {
         int receivedValue = percent;
-        int calculatedValue = 100 - percent;
+        int calculatedValue = new Random().nextInt(100);
         return (receivedValue > calculatedValue) ? true : false;
     }
 }
