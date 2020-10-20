7a92ac3168db93f050febd1ccead7c1fa814af48
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-10-20 18:45:21 +0300

    homework 7 alltask partialy or partially working

--- src/main/homework7/task4/AnnuityCredit.java
+++ src/main/homework7/task4/AnnuityCredit.java
@@ -1,4 +1,19 @@
 package main.homework7.task4;
 
-public class AnnuityCredit {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnnuityCredit extends BaseCredit {
+
+    @Override
+    public List<Double> getMonthPayments(){
+        double ratePerMonth=rate/100/12;
+        List<Double> paymentsList=new ArrayList<>();
+        double monthlyPayment=amount*ratePerMonth*Math.pow(1+ratePerMonth,duration)/(Math.pow(1+ratePerMonth,duration)-1);
+        for(int j=0;j<duration;j++){
+            paymentsList.add(monthlyPayment);
+        }
+        return paymentsList;
+    }
+
 }
--- src/main/homework7/task4/BaseCredit.java
+++ src/main/homework7/task4/BaseCredit.java
@@ -1,7 +1,24 @@
 package main.homework7.task4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseCredit {
     public double amount,rate;
     int duration;
+
+
+    public List<Double> getMonthPayments(){
+        List<Double> paymentsList=new ArrayList<>();
+        for (int i=0;i<duration;i++){
+            double monthPayment=amount/duration;
+            paymentsList.add(monthPayment);
+        }
+        return paymentsList;
+    }
+    public Double calculateOverpayment(){
+        double overPayment=rate*(amount/duration);
+        return overPayment;
+    }
 
 }
