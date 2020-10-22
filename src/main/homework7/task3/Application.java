9f40bf121aba951b4f5cbccf8b725ff83aabd425
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-22 23:12:11 +0300

    homework 7 alltask partialy or partially working

--- src/main/homework7/task3/Application.java
+++ src/main/homework7/task3/Application.java
@@ -1,5 +1,7 @@
 package main.homework7.task3;
 
+import main.homework7.task3.Figures.ChessFigure;
+import main.homework7.task3.Figures.King;
 import main.homework7.task3.chess.Chess;
 import main.homework7.task3.chess.Position;
 
@@ -7,10 +9,8 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-       Set<Position> allPositions= Chess.getAll();
-        for (Position pos:allPositions) {
-            System.out.println(pos);
-        }
-        System.out.println(allPositions.size());
+        Position position=new Position("A4");
+        ChessFigure figure=new King(position);
+        figure.getAvaibleMoves();
     }
 }
