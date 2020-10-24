e7258d9748fc43ee7954d09820db1bf3ef12a165
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-24 15:46:40 +0300

    homework 8-start

---
+++ src/main/homework8/task1/Person.java
@@ -0,0 +1,31 @@
+package main.homework8.task1;
+
+import main.homework6.task4.IdentityDocument;
+import main.homework6.task4.NameGenerator;
+
+import java.util.Random;
+
+public class Person {
+    private String surname;
+    private String name;
+
+    public Person() {
+    surname="Фамилия-"+numberGenerate();
+    name="Имя-"+numberGenerate();
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static int numberGenerate() {
+        int number = new Random().nextInt(20) + 1;
+        return number;
+    }
+}
+
