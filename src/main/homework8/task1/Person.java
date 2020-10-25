29f572f3ecbc05c35d81eb402483761ecd37692e
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-25 12:37:33 +0200

    homework 8-start

--- src/main/homework8/task1/Person.java
+++ src/main/homework8/task1/Person.java
@@ -3,6 +3,7 @@ package main.homework8.task1;
 import main.homework6.task4.IdentityDocument;
 import main.homework6.task4.NameGenerator;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Person {
@@ -26,6 +27,22 @@ public class Person {
     public static int numberGenerate() {
         int number = new Random().nextInt(20) + 1;
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        if(!Objects.equals(surname,person.surname)) return false;
+        return Object.equals(name,person.name);
+    }
+
+    @Override
+    public int hashCode() {
+       int result=surname!=null?surname.hashCode():0;
+       result=31*result+(name!=null?name.hashCode():0);
+       return result;
     }
 
     @Override
