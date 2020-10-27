b3db3e79d6fcfdb96642169aaec89db3c5cbdc51
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-27 20:03:51 +0200

    homework 8-1 and 3 done

--- src/main/homework8/task3/Application.java
+++ src/main/homework8/task3/Application.java
@@ -1,7 +1,19 @@
 package main.homework8.task3;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        List<Terminal> list= Arrays.asList(
+                new QiwiTerminal("г. Москва, ул. Проходчиков, д.3"),
+                new EleksnetTerminal("г. Москва, Измайловский пр-т, 73/2"),
+                new EleksnetTerminal("г. Москва, ул. Бакунинская, д.1"),
+                new EleksnetTerminal("г. Новосибирск, ул. Героев-Десантников, д. 4"),
+                new TinkoffTerminal("г. Москва, Ленинградский пр-т, д. 15")
+                 );
+
+        for (Terminal terminal:list) terminal.pay("+79090224489",200);
 
     }
 }
