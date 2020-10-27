bdb53d904700ceea7ea2bb288deaa734935e2c4b
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-27 19:43:37 +0200

    homework 8-1 done++

--- src/main/homework8/task3/EleksnetTerminal.java
+++ src/main/homework8/task3/EleksnetTerminal.java
@@ -1,4 +1,10 @@
 package main.homework8.task3;
 
-public class EleksnetTerminal {
+public class EleksnetTerminal extends Terminal {
+    public EleksnetTerminal(String address) {
+        super(address);
+        owner=Owner.ELEKSNET;
+        commision=11;
+        withoutCommision=300;
+    }
 }
