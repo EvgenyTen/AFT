5f63be77c0074beb53e739ed52ff288614e8b36d
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-29 19:52:40 +0200

    homework 9 task1

--- src/main/homework9/task1/Factory.java
+++ src/main/homework9/task1/Factory.java
@@ -3,7 +3,7 @@ package main.homework9.task1;
 import java.util.Random;
 
 public class Factory {
-    public static Object make() {
+    public static Car make() {
         int rand = new Random().nextInt(101);
         if (rand >= 0 && rand < 40) return new Renault();
         else if (rand >= 40 && rand < 70) return new Nissan();
