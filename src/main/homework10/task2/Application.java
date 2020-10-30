b87885d9c3ec0aa2517c75f0b1b86eb30a529647
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-30 20:05:46 +0200

    homework 10 task 1

--- src/main/homework10/task2/Application.java
+++ src/main/homework10/task2/Application.java
@@ -1,15 +1,15 @@
 package main.homework10.task2;
 
-import java.io.ObjectInputFilter;
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<String> generatedContent=new ArrayList<>();
+        List generatedContent=new ArrayList<>();
+
         for(int i = 0; i< Config.LINES_AMOUNT; i++){
             generatedContent.add(StringHelper.generate());
         }
-        FileHelper(generatedContent);
+        FileHelper.write(generatedContent);
     }
 }
--- src/main/homework10/task2/StringHelper.java
+++ src/main/homework10/task2/StringHelper.java
@@ -1,4 +1,15 @@
 package main.homework10.task2;
 
+import java.util.Random;
+
 public class StringHelper {
+    public static String generate() {
+        int length = new Random().nextInt(Config.LINES_MAX_LENGTH - Config.LINES_MIN_LENGTH + 1) + Config.LINES_MIN_LENGTH;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++)
+            builder.append(Config.LINES_PATTERN.charAt(new Random().nextInt(Config.LINES_PATTERN.length())));
+        String result = builder.toString();
+        System.out.println(Config.LINES_MESSAGE, result);
+        return result;
+    }
 }
