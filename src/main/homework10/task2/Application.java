6fb3047b83af65da9dbd8c1a4a91eb2a3c220c87
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-30 20:56:40 +0200

    homework 10 task 2

--- src/main/homework10/task2/Application.java
+++ src/main/homework10/task2/Application.java
@@ -3,6 +3,7 @@ package main.homework10.task2;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Application {
     public static void main(String[] args) {
         List<String> generatedContent = new ArrayList<>();
