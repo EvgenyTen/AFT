b3b21f14bbd796c0713069b7d361c0c0fd18d947
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-30 19:18:28 +0200

    homework 10 task 1

--- src/main/homework10/task2/Application.java
+++ src/main/homework10/task2/Application.java
@@ -1,6 +1,15 @@
 package main.homework10.task2;
+
+import java.io.ObjectInputFilter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-
+        List<String> generatedContent=new ArrayList<>();
+        for(int i = 0; i< Config.LINES_AMOUNT; i++){
+            generatedContent.add(StringHelper.generate());
+        }
+        FileHelper(generatedContent);
     }
 }
---
+++ src/main/homework10/task2/Config.java
@@ -0,0 +1,4 @@
+package main.homework10.task2;
+
+public class Config {
+}
