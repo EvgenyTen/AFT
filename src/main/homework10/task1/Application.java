769664d8a9672e8345b10b6a27b5532f0d970756
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-30 18:05:42 +0200

    homework 10 start

--- src/main/homework10/task1/Application.java
+++ src/main/homework10/task1/Application.java
@@ -1,6 +1,15 @@
 package main.homework10.task1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        File inputFile=new File("src\\main\\homework10\\task1\\input.txt");
+        File output=new File ("src\\main\\homework10\\task1\\output.txt");
+        Map<String,String> content=FileHelper.readFile(inputFile);
+        FileHelper.saveFile(output,content);
 
     }
 }
---
+++ src/main/homework10/task1/FileHelper.java
@@ -0,0 +1,13 @@
+package main.homework10.task1;
+
+import java.io.File;
+import java.util.Map;
+
+public class FileHelper {
+    public static Map<String, String> readFile(File file) {
+        return null;
+    }
+    public static void saveFile(File file,List<>()){
+
+    }
+}
