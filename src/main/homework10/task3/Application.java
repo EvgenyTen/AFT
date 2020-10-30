1d6f3f846cdaf5305055685bdc9e6fac96416b24
Evgeny <evgeny.tenenbaum#gmail.com>
2020-10-30 20:54:35 +0200

    homework 10 task 2

--- src/main/homework10/task3/Application.java
+++ src/main/homework10/task3/Application.java
@@ -1,7 +1,11 @@
 package main.homework10.task3;
 
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Generator generator=new FileGen(10);
+        generator.generate();
 
     }
 }
