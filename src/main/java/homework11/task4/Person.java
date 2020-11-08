d1f79b7a328d357601d0f0c8186ca336186536b0
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-08 21:52:02 +0200

    homework 11-after task 2

--- src/main/java/homework11/task4/Person.java
+++ src/main/java/homework11/task4/Person.java
@@ -1,4 +1,65 @@
 package homework11.task4;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Person {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private Date birthDate;
+    private Passport passport;
+
+    public Person(String lastName, String name, String patronymic, Date birthDate, Passport passport) {
+        this.surname = lastName;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.birthDate = birthDate;
+        this.passport = passport;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (!Objects.equals(surname, person.surname)) return false;
+        if (!Objects.equals(name, person.name)) return false;
+        if (!Objects.equals(patronymic, person.patronymic)) return false;
+        if (!Objects.equals(birthDate, person.birthDate)) return false;
+        return Objects.equals(passport, person.passport);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = surname != null ? surname.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (patronymic != null ? patronymic.hashCode() : 0);
+        result = 31 * result + (birthDate != null ? birthDate.hashCode() : 0);
+        result = 31 * result + (passport != null ? passport.hashCode() : 0);
+        return result;
+    }
 }
+
