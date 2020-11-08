fd785065a1e08cdee15addeadb0064e140adf1cb
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-08 22:12:13 +0200

    homework 11-after task 3

---
+++ src/test/java/homework11/task3/RandomString.java
@@ -0,0 +1,19 @@
+package homework11.task3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class RandomString {
+    final int AMOUNT=10000;
+    @Test
+    public void stringsTest(){
+        for(int i=0;i<AMOUNT;i++){
+            int from=new Random().nextInt(20);
+            int to=20+new Random().nextInt(20);
+            String rand=Generator.randomString(from,to);
+            Assertions.assertTrue(rand.matches("^[A-Za-z]{0,40}$"));
+        }
+    }
+}
