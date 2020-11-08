39cae10fa2e51e9167bc325f812867028727b076
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-08 17:19:20 +0200

    homework 11-task 0

---
+++ src/main/java/homework11/task1/Calculator.java
@@ -0,0 +1,15 @@
+package homework11.task1;
+
+public class Calculator {
+    public static int calculate(int... values) {
+        int sum = 0;
+        for (int val : values) sum += val;
+        return sum;
+    }
+
+    public static int multiplay(int... values) {
+        int res = 0;
+        for (int val : values) res *= val;
+        return res;
+    }
+}
