ed602a427a33c774d7638fe633fcdcfa99871788
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-20 15:02:59 +0200

    homework 12-somethere in task 1

--- src/test/java/homework12/task1/XmlDeserialize.java
+++ src/test/java/homework12/task1/XmlDeserialize.java
@@ -1,4 +1,13 @@
 package homework12.task1;
 
+import homework12.task1.helpers.JsonHelper;
+
 public class XmlDeserialize {
+    private final JsonHelper help=new JsonHelper();
+    private final String ROOM_PATH="src\\test\\java\\homework12\\task1\\files\\room.json";
+    private final String FLAT_PATH="src\\test\\java\\homework12\\task1\\files\\flat.json";
+    private final String HOUSE_PATH="src\\test\\java\\homework12\\task1\\files\\house.json";
+
+
+
 }
