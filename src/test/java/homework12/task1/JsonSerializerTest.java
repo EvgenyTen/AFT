735c53e16d6cdf4197b9d30d470d14bd29d1b2f9
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-20 14:16:49 +0200

    homework 12-somethere in task 1

---
+++ src/test/java/homework12/task1/JsonSerializerTest.java
@@ -0,0 +1,21 @@
+package homework12.task1;
+
+import homework12.task1.helpers.JsonHelper;
+import homework12.task1.model.House;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonSerializerTest {
+  private final JsonHelper help=new JsonHelper();
+  private final String PATH="src/test/java/homework12/task1/files/expected_json.txt";
+  @Test
+  void serializationTest() throws IOException {
+      House house=new House().generate();
+      String actual=help.serialize(house);
+      String expected= Files.readAllLines(Paths.get(PATH)).get(0);
+      Assertions.assertEquals(expected,actual);
+  }
+}
---
+++ src/test/java/homework12/task1/XmlSerializeTest.java
@@ -0,0 +1,4 @@
+package homework12.task1;
+
+public class XmlSerializeTest {
+}
