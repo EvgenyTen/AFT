7df23b7e54281e01789a260b067574c9eff34b4a
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-20 14:38:04 +0200

    homework 12-somethere in task 1

--- src/test/java/homework12/task1/JsonDeserializationTest.java
+++ src/test/java/homework12/task1/JsonDeserializationTest.java
@@ -1,4 +1,11 @@
 package homework12.task1;
 
+import homework12.task1.helpers.JsonHelper;
+
 public class JsonDeserializationTest {
+    private final JsonHelper help=new JsonHelper();
+    private final String ROOM_PATH=
+    private final String FLAT_PATH=
+    private final String HOUSE_PATH=
+
 }
