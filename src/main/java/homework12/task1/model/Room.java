f025cd27c065bd9967d8b8d7d20bc7931bb2f062
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-20 13:00:07 +0200

    homework 12-somethere in task 1

--- src/main/java/homework12/task1/model/Room.java
+++ src/main/java/homework12/task1/model/Room.java
@@ -1,4 +1,12 @@
 package homework12.task1.model;
 
-public class Room {
+public class Room  implements Generatable{
+    private int width;
+    private int height;
+    public Room(){}
+    public Room(int width,int height){this.width=width ;this.height=height;}
+    public int getWitdh() {return width;}
+    public int getHeight(){return height;}
+    @Override
+    public Room generate(){width=5;height=3;return this;}
 }
