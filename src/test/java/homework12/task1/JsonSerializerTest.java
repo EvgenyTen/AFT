bc359d4862411acefbaf6892fe41a4016fe5d5df
EvgenyTen <evgeny.tenenbaum@gmail.com>
2020-11-20 14:30:11 +0200

    homework 12-somethere in task 1

--- src/test/java/homework12/task1/JsonSerializerTest.java
+++ src/test/java/homework12/task1/JsonSerializerTest.java
@@ -3,6 +3,7 @@ package homework12.task1;
 import homework12.task1.helpers.JsonHelper;
 import homework12.task1.model.House;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,6 +13,7 @@ public class JsonSerializerTest {
   private final JsonHelper help=new JsonHelper();
   private final String PATH="src/test/java/homework12/task1/files/expected_json.txt";
   @Test
+  @DisplayName("Object Serialization Test")
   void serializationTest() throws IOException {
       House house=new House().generate();
       String actual=help.serialize(house);
--- src/test/java/homework12/task1/XmlSerializeTest.java
+++ src/test/java/homework12/task1/XmlSerializeTest.java
@@ -1,4 +1,22 @@
 package homework12.task1;
+import homework12.task1.helpers.XmlHelper;
+import homework12.task1.model.House;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class XmlSerializeTest {
+    private final XmlHelper help=new XmlHelper();
+    private final String PATH="src/test/java/homework12/task1/files/expected_xml.txt";
+
+    @Test
+    @DisplayName("Object Serialization Test")
+    void xmlSerializationTest () throws IOException{
+        House house =new House().generate();
+        String actual=help.serialize(house);
+        String expected= Files.readAllLines(paths.get(PATH)).get(0);
+        Assertions.assertEquals(expected,actual);
+    }
 }
