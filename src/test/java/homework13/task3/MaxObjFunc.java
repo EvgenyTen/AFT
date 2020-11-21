885921e4d1c02e5fb7eb329e44e9e84a034cb475
Evgen <evgeny.tenenbaum@gmail.com>
2020-11-21 18:02:04 +0200

    homework 13-task 3 in middle2

--- src/test/java/homework13/task3/MaxObjFunc.java
+++ src/test/java/homework13/task3/MaxObjFunc.java
@@ -1,4 +1,17 @@
 package homework13.task3;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class MaxObjFunc {
+    @Test
+    @DisplayName("object-string")
+    void maxObjString() { Assertions.assertEquals("zvcv",Functions.maxObj.execute(Arrays.asList("abcd","zvcv","qwwj")));}
+
+    @Test
+    @DisplayName("objct-doubles")
+    void maxObjDoubles() { Assertions.assertEquals(258.15,Functions.maxObj.execute(Arrays.asList(11.78,258.15,192.34)));}
 }
