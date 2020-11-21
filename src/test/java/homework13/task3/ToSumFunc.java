1beab062c63864d8414527f2f97b88f6d28a0f5e
Evgen <evgeny.tenenbaum@gmail.com>
2020-11-21 17:58:29 +0200

    homework 13-task 3 in middle2

--- src/test/java/homework13/task3/ToSumFunc.java
+++ src/test/java/homework13/task3/ToSumFunc.java
@@ -1,4 +1,14 @@
 package homework13.task3;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class ToSumFunc {
+
+        @Test
+        @DisplayName("sum test")
+        void sumFunc() { Assertions.assertEquals(55,Functions.sumFunc.execute(Arrays.asList(1,2,3,4,5,10,9,8,7,6)));}
 }
