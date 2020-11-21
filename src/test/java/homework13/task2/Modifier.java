0fe3543594af2061ed20d1b7c42865f0a5615e14
Evgen <evgeny.tenenbaum@gmail.com>
2020-11-21 12:06:41 +0200

    homework 13-task 2 finished

---
+++ src/test/java/homework13/task2/Modifier.java
@@ -0,0 +1,26 @@
+import homework13.task2.Modifiers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Modifier {
+
+    @Test
+    @DisplayName("Boolean")
+    void testBoolean (){
+        Assertions.assertFalse(Modifiers.invert.modify(true));
+        Assertions.assertTrue(Modifiers.invert.modify(false));
+    }
+    @Test
+    @DisplayName("Integer")
+    void testInteger(){
+        Assertions.assertEquals(5,Modifiers.absolute.modify(5));
+        Assertions.assertEquals(5,Modifiers.absolute.modify(-5));
+    }
+
+    @Test
+    @DisplayName("String")
+    void testString(){
+        Assertions.assertEquals("abcdef",Modifiers.reverse.modify("fedcba"));
+    }
+}
