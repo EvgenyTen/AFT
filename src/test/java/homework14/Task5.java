f10862e6e5d542b6227e46a8b14a10b1888b9063
Evgen <evgeny.tenenbaum@gmail.com>
2020-11-28 20:47:48 +0200

    homework 14-All except number 5

---
+++ src/test/java/homework14/Task5.java
@@ -0,0 +1,36 @@
+package homework14;
+
+import homework14.helpers.JsonHelper;
+import homework14.model.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class Task5 {
+    List<Person> persons = JsonHelper.getPersonsFromFile("src/test/java/homework14/dataset.json");
+    @Test
+    @DisplayName("Task 1")
+    public void task1() {
+
+        persons.stream()
+                .filter(person -> !person.getAccounts().isEmpty())
+                .filter(person -> person.getAccounts().stream()
+                        .map(Account->Account.getAccountBalance())
+                        .reduce((balance1, balance2) -> balance1 + balance2)
+                        .orElseThrow(()->new IllegalStateException())<=2100000)
+                .forEach(person -> System.out.printf("%s %s %s%n",person.getLastName(),person.getFirstName(),person.getPatronymic()));
+    }
+    @Test
+    @DisplayName("Task 2")
+    public void task2() {
+
+        persons.stream()
+                .filter(person -> !person.getAccounts().isEmpty())
+                .filter(person -> person.getAccounts().stream()
+                        .map(Account->Account.getAccountBalance())
+                        .reduce((balance1, balance2) -> balance1 + balance2)
+                        .orElseThrow(()->new IllegalStateException())<=2100000)
+                .forEach(person -> System.out.printf("%s %s %s%n",person.getLastName(),person.getFirstName(),person.getPatronymic()));
+    }
+}
